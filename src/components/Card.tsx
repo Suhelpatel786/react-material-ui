aec044a25a02668c37cdb0d3c1d90fc91edb3e8a
Suhel Patel <suhel.softcolon@gmail.com>
2024-02-08 16:45:12 +0530

    books page

---
+++ src/components/Card.tsx
@@ -0,0 +1,75 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Box, CardActionArea } from "@mui/material";
+import { book1 } from "../assets";
+
+interface cardProps {
+  item?: any;
+}
+
+export default function ActionAreaCard({ item }: cardProps) {
+  return (
+    <Card
+      sx={{
+        minWidth: { xs: 300, md: 345 },
+        maxWidth: { xs: 300, md: 345 },
+        mb: { xs: "1rem", sm: "3rem", md: "5rem" },
+      }}
+    >
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={item.img}
+          sx={{
+            objectFit: "cover",
+            objectPosition: "center",
+            minHeight: 341,
+            maxHeight: 341,
+          }}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+              alignItems: "start",
+              justifyContent: "center",
+            }}
+          >
+            <Typography
+              gutterBottom
+              variant="h5"
+              sx={{ fontWeight: "600" }}
+              component="div"
+            >
+              {item?.name?.length >= 17
+                ? item?.name?.substr(0, 18) + "..."
+                : item?.name}
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="body1"
+              sx={{ fontWeight: "600" }}
+              component="div"
+            >
+              {item?.author}
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="body2"
+              sx={{ fontWeight: "600" }}
+              component="div"
+            >
+              {item?.price}
+            </Typography>
+          </Box>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,7 +1,31 @@
+import { Box } from "@mui/material";
 import React from "react";
+import CardComponent from "../components/Card";
+
+import { bookDetails } from "../constants";
 
 const Books = () => {
-  return <div>Books Page</div>;
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: {
+          xs: "center",
+          sm: "space-evenly",
+          md: "space-between",
+        },
+        alignItems: "center",
+        px: { xs: "0", sm: "1rem" },
+        pt: "2rem",
+      }}
+    >
+      {bookDetails.map((item: any) => (
+        <CardComponent key={item.id} item={item} />
+      ))}
+    </Box>
+  );
 };
 
 export default Books;
