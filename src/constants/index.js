07cf6d4e60bec5aa62961b51b3f97fe5c24e6324
Suhel Patel <suhel.softcolon@gmail.com>
2024-02-08 09:02:45 +0530

    books detail

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,17 @@
-// export const navItems = ["Home", "Books", "Blogs", "Contact"];
+import {
+  book1,
+  book10,
+  book11,
+  book12,
+  book2,
+  book3,
+  book4,
+  book5,
+  book6,
+  book7,
+  book8,
+  book9,
+} from "../assets";
 
 export const navItems = [
   {
@@ -23,3 +36,114 @@ export const colors = {
   backGroundColor: "#fffff0",
   appBarBackgroundColor: "#008080",
 };
+
+export const bookDetails = [
+  {
+    id: 1,
+    img: book1,
+    name: "Spring is in the Air",
+    author: "Jane Smith",
+    price: "$14.99",
+    detail:
+      "Spring is in the Air is a heartwarming tale of love, hope, and new beginnings. Set in the picturesque countryside, this enchanting story follows the lives of Sarah and David as they navigate the joys and challenges of springtime. From blossoming flowers to chirping birds, the beauty of the season mirrors the blossoming romance between the two protagonists. With its charming characters and evocative storytelling, Spring is in the Air is a delightful read that captures the essence of springtime magic.",
+  },
+  {
+    id: 2,
+    img: book2,
+    name: "Reduce Reuse Recycle",
+    author: "Alex Green",
+    price: " $12.49",
+    detail:
+      "Reduce Reuse Recycle is an inspiring guide to living a more sustainable life. Authored by environmentalist Alex Green, this book provides practical tips and insights on how to reduce waste, minimize environmental impact, and promote eco-friendly habits. From simple lifestyle changes to innovative recycling techniques, the book offers actionable advice for individuals and families looking to make a positive difference for the planet. With its informative content and engaging writing style, Reduce Reuse Recycle empowers readers to embrace sustainability and become agents of change in their communities.",
+  },
+  {
+    id: 3,
+    img: book3,
+    name: "Love Work: The Seven Steps to Thrive at Work",
+    author: "Sarah Johnson",
+    price: "$15.99",
+    detail:
+      "Love Work: The Seven Steps to Thrive at Work is a transformative guide for finding joy and fulfillment in your professional life. Authored by Sarah Johnson, a renowned career coach, this book presents a comprehensive approach to achieving success and happiness in the workplace. Through seven actionable steps, readers will learn how to cultivate a positive mindset, build meaningful relationships, and unlock their full potential at work. With practical exercises, inspiring anecdotes, and expert advice, Love Work empowers individuals to create a work environment where passion, purpose, and productivity thrive. Whether you're seeking career advancement, greater satisfaction, or simply a more enjoyable work experience, this book offers invaluable insights to help you love what you do and excel in your career.",
+  },
+  {
+    id: 4,
+    img: book4,
+    name: "The Passion Within: A Women's Journal",
+    author: "Emily Hart",
+    price: "$9.99",
+    detail:
+      "The Passion Within: A Women's Journal is a beautifully crafted journal designed to inspire and empower women on their journey of self-discovery and personal growth. Authored by Emily Hart, a renowned life coach and motivational speaker, this journal provides a safe space for women to explore their passions, dreams, and desires. With thought-provoking prompts, uplifting quotes, and empowering affirmations, The Passion Within encourages women to tap into their inner strength, embrace their uniqueness, and pursue their passions with confidence and purpose. Whether used as a daily reflection tool or as a companion for personal development, this journal is a powerful resource for women seeking to unlock their potential and live a life filled with passion and purpose.",
+  },
+  {
+    id: 5,
+    img: book5,
+    name: "The Catcher in the Ray",
+    author: "J.D. Salinger",
+    price: "$11.99",
+    detail:
+      "The Catcher in the Ray is a timeless classic written by J.D. Salinger. This iconic novel follows the story of Holden Caulfield, a disillusioned teenager navigating the complexities of adolescence and society in post-World War II America. Through Holden's introspective journey, readers explore themes of identity, alienation, and the search for authenticity in a world filled with phoniness. With its compelling narrative and poignant insights, The Catcher in the Ray continues to resonate with readers of all ages, making it a must-read for anyone seeking to understand the human experience and the challenges of growing up in a changing world.",
+  },
+  {
+    id: 6,
+    img: book6,
+    name: "Anime and Action: A Visual Journey",
+    author: "A. J. Turner",
+    price: "$14.99",
+    detail:
+      "Explore the dynamic world of anime and action cartoons in Anime and Action: A Visual Journey. This captivating book takes readers on an immersive tour of the vibrant and exhilarating realms depicted in anime and action cartoons. From epic battles to heartwarming friendships, discover the artistry and creativity that bring these animated worlds to life. Immerse yourself in colorful illustrations, behind-the-scenes anecdotes, and fascinating trivia, celebrating the magic of animation. Join us on this thrilling adventure through the realms of imagination, where every page is filled with excitement and wonder.",
+  },
+  {
+    id: 7,
+    img: book7,
+    name: "Your Soul is a River",
+    author: "A. J. Rivers",
+    price: "$10.99",
+    detail:
+      "Your Soul is a River is a soul-stirring journey through the depths of the human experience. Authored by A. J. Rivers, this poignant book navigates the ebb and flow of life's emotions, offering solace, inspiration, and reflection along the way. With lyrical prose and heartfelt insights, embark on a transformative exploration of love, loss, resilience, and self-discovery. Each page is a ripple in the river of life, inviting readers to dive deep into the currents of their own souls. Let this book be your guide as you navigate the twists and turns of existence, finding meaning and purpose in every bend of the river.",
+  },
+  {
+    id: 8,
+    img: book8,
+    name: "Courageous Pursuits: Embracing Fear and Unlocking Success",
+    author: "Harper Quinn",
+    price: "$11.99",
+    detail:
+      "Courageous Pursuits: Embracing Fear and Unlocking Success is a compelling exploration of fear's role in personal growth and achievement. Written by Harper Quinn, this insightful book challenges readers to confront their fears head-on and pursue their dreams with courage and determination. Through real-life stories, practical exercises, and valuable insights, Courageous Pursuits equips readers with the tools they need to overcome obstacles, take risks, and achieve their goals. Whether you're starting a new venture, facing a career change, or pursuing a lifelong dream, this book will inspire you to embrace fear as a catalyst for success. Join Harper Quinn on a journey of self-discovery and empowerment, and unlock your full potential in every aspect of life.",
+  },
+  {
+    id: 9,
+    img: book9,
+    name: "Dear Stranger: Understanding Emotions and Connecting with Others",
+    author: "Riley James",
+    price: "$9.99",
+    detail:
+      "Dear Stranger: Understanding Emotions and Connecting with Others is a compassionate exploration of empathy and human connection. Authored by Riley James, this heartfelt book delves into the complexities of emotions and the power of empathy to bridge the gap between strangers. Through heartfelt anecdotes, insightful reflections, and practical exercises, Dear Stranger encourages readers to embrace vulnerability, cultivate empathy, and forge deeper connections with those around them. Whether you're navigating relationships, seeking to understand others, or simply looking to connect on a deeper level, this book offers valuable insights and guidance. Join Riley James on a journey of emotional discovery and learn how to truly understand and connect with the strangers in your life.",
+  },
+  {
+    id: 10,
+    img: book10,
+    name: "Moments Gathered: Reflections on Life's Journey",
+    author: "Lily Johnson",
+    price: "$12.99",
+    detail:
+      "Moments Gathered: Reflections on Life's Journey is a heartfelt collection of musings and memories. Authored by Lily Johnson, this evocative book invites readers to pause and reflect on the beauty and complexity of life's journey. Through poignant stories, heartfelt reflections, and timeless wisdom, Moments Gathered captures the essence of the human experience and the power of gathering moments along the way. Whether you're savoring the simple joys of everyday life or navigating the challenges of change and growth, this book offers solace, inspiration, and a gentle reminder to cherish each moment as it unfolds. Join Lily Johnson on a journey of discovery, and let Moments Gathered illuminate your path with its warmth and wisdom.",
+  },
+  {
+    id: 11,
+    img: book11,
+    name: "The Code of Justice: Navigating the Legal Landscape",
+    author: "Benjamin Lawson",
+    price: "$14.99",
+    detail:
+      "The Code of Justice: Navigating the Legal Landscape is a comprehensive guide to understanding the principles and practices of law. Authored by Benjamin Lawson, a seasoned legal expert, this book provides readers with valuable insights into the complexities of the legal system. From the foundations of law to the intricacies of courtroom procedure, The Code of Justice covers a wide range of topics essential for navigating the legal landscape. Whether you're a student, a professional, or simply curious about the law, this book offers clear explanations, practical advice, and real-life examples to help you understand and navigate the complexities of the legal world. Join Benjamin Lawson on a journey through the corridors of justice, and gain the knowledge and confidence to navigate legal challenges with clarity and integrity.",
+  },
+  {
+    id: 12,
+    img: book12,
+    name: "Dino Delights: A Journey into the World of Adorable Dinosaurs",
+    author: "Emma Clarke",
+    price: "$9.99",
+    detail:
+      "Dino Delights: A Journey into the World of Adorable Dinosaurs is a delightful exploration of the fascinating world of cute dinosaur toys. Authored by Emma Clarke, this charming book captures the whimsy and wonder of these prehistoric creatures brought to life in toy form. From cuddly plushies to whimsical figurines, Dino Delights showcases a colorful array of dinosaur toys that will capture the hearts of readers of all ages. Each page is filled with adorable illustrations, fun facts, and imaginative play ideas, making this book a perfect companion for dinosaur enthusiasts and toy lovers alike. Join Emma Clarke on a journey through the land of cute dinosaurs, and let Dino Delights spark your imagination and reignite your sense of wonder.",
+  },
+];
